c127b2f4c8bd8c02d9125a0a309c3745e07a270e
MeezaanD <meezaandavids365@gmail.com>
2025-05-07 18:31:10 +0200

    fix: retest deployment

--- firebase.js
+++ firebase.js
@@ -1,10 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+// import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+// import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
 
-// Use global firebaseConfig from config.js
-const app = initializeApp(window.firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+// const app = initializeApp(window.firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+// export const storage = getStorage(app);
+
+
+// Initialize Firebase with window.firebaseConfig
+firebase.initializeApp(window.firebaseConfig);
+
+// Optional: Initialize services
+const auth = firebase.auth();
+const db = firebase.firestore();
